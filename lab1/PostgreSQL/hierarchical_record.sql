da7ea90158865d63a31affc673dbbf3f943e4dc2
FryMondo <ptachyksergei@gmail.com>
2026-03-29 23:29:17 +0300

    lab 1 (PostgreSQL)

---
+++ lab1/PostgreSQL/hierarchical_record.sql
@@ -0,0 +1,13 @@
+WITH RECURSIVE employee_tree AS (
+    SELECT id, first_name, last_name, manager_id, 1 AS level
+    FROM employees
+    WHERE id = 1
+
+    UNION ALL
+
+    SELECT e.id, e.first_name, e.last_name, e.manager_id, et.level + 1
+    FROM employees e
+             JOIN employee_tree et ON e.manager_id = et.id
+)
+SELECT LPAD(' ', (level - 1) * 2) || first_name || ' ' || last_name AS employee_name, level
+FROM employee_tree;
